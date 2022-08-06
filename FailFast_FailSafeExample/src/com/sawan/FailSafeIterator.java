0824fd6a0b96962a8ece0cc14882770d5807ee78
dhamrendrasawan <dhamrendrasawan@gmail.com>
2022-08-06 20:10:14 +0530

    Add files via upload

---
+++ FailFast_FailSafeExample/src/com/sawan/FailSafeIterator.java
@@ -0,0 +1,33 @@
+package com.sawan;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+//creating FailSafeIterator.java class   
+public class FailSafeIterator {
+	public static void main(String[] args) {
+		//creating map  
+        Map<Integer, String> students = new HashMap<Integer, String>();   
+        students.put(101, "Emma");   
+        students.put(102, "Paul");   
+        students.put(103, "Walker");  
+          
+        //creating ConcurrentHashMap by copy students map   
+        ConcurrentHashMap<Integer, String> copyStudents = new ConcurrentHashMap<Integer, String>(students);   
+          
+        //creating an instance of the Iterator class  
+        Iterator itr = copyStudents.keySet().iterator();   
+          
+        //iterating map using Iterator   
+        while (itr.hasNext()) {  
+            //printing elements of copyStudents  
+            System.out.println(copyStudents.get(itr.next()));   
+            // adding an element to copyStudents   
+            copyStudents.put(104, "Rubby"); // on the next call of next() method, exception will not be thrown.  
+        }   	
+    
+	}
+
+}
---
+++ FailFast_FailSafeExample/src/com/sawan/FailSafeIterator2.java
@@ -0,0 +1,30 @@
+package com.sawan;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class FailSafeIterator2 {
+	public static void main(String[] args) {		
+		//creating ArrayList  
+        ArrayList<String> students = new ArrayList<String>();   
+        students.add("Emma");   
+        students.add("Paul");   
+        students.add("Walker");  
+          
+        //creating CopyOnWriteArrayList by copy students ArrayList   
+        CopyOnWriteArrayList<String> copyStudents = new CopyOnWriteArrayList<String>(students);   
+          
+        //creating an instance of the Iterator class  
+        Iterator itr = copyStudents.iterator();   
+          
+        //iterating CopyOnWriteArrayList using Iterator   
+        while (itr.hasNext()) {  
+            String name = (String)itr.next();   
+            System.out.println(name);   
+            if (name == "Paul")   
+                // The Rubby name will not print but it has created separate copy   
+                copyStudents.add("Rubby");
+        }
+	}
+}
