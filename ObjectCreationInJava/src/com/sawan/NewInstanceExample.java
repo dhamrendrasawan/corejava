63550b0be50a240054e7cb6f5a27b7a3d1069bbc
dhamrendrasawan <dhamrendrasawan@gmail.com>
2022-08-06 20:35:47 +0530

    Add files via upload

---
+++ ObjectCreationInJava/src/com/sawan/NewInstanceExample.java
@@ -0,0 +1,18 @@
+package com.sawan;
+
+public class NewInstanceExample {
+	
+	String str="hello"; 
+	public static void main(String[] args) {
+		 try  
+         {              //creating object of class  
+                NewInstanceExample obj= NewInstanceExample.class.newInstance();   
+                System.out.println(obj.str);          
+           }  
+          catch(Exception e)  
+          {  
+              e.printStackTrace();  
+           }  
+	}
+
+}
---
+++ ObjectCreationInJava/src/com/sawan/NewInstanceExample1.java
@@ -0,0 +1,18 @@
+package com.sawan;
+import java.lang.reflect.Constructor;
+public class NewInstanceExample1 {
+
+	String str="hello";
+	public static void main(String[] args) {        
+	   try  
+	   {  
+		   Constructor<NewInstanceExample1> obj =NewInstanceExample1.class.getConstructor();  
+		   NewInstanceExample1 obj1 = obj.newInstance();  
+		   System.out.println(obj1.str);  
+		}  
+		catch(Exception e)  
+		{  
+		    e.printStackTrace();  
+		}
+	}
+}
