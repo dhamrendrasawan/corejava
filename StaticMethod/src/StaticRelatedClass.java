30327d2b19e47b95a3f952669075d0828cf6c3a3
dhamrendrasawan <dhamrendrasawan@gmail.com>
2022-08-06 20:40:15 +0530

    Add files via upload

---
+++ StaticMethod/src/StaticRelatedClass.java
@@ -0,0 +1,17 @@
+
+public class StaticRelatedClass {
+
+	public static void show(int x)
+	{
+		System.out.println("static show");
+	}
+	 void show(int x1)
+	{
+		System.out.println("static show");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
